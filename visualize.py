a176828040936a60968faeb551a11aa8efd727ff
jacobortiz <jacobortiz90@gmail.com>
2024-03-29 13:19:21 -0700

    updated visualize.py

--- visualize.py
+++ visualize.py
@@ -24,34 +24,34 @@ if __name__ == '__main__':
 
         
     # load the model
-    # file = 'baseline-ABC-K_1-C_1-beta_1'
+    file = 'baseline/baseline-ABC-K_20-C_1-beta_1-N_100k'
 
-    # loaded_model = bz2.BZ2File(f'data/{file}.pbz2', 'rb')
-    # loaded_model = pickle.load(loaded_model)
+    loaded_model = bz2.BZ2File(f'data/{file}.pbz2', 'rb')
+    loaded_model = pickle.load(loaded_model)
 
-    # loaded_model.info()
+    loaded_model.info()
 
-    # # convergence time
-    # print(f'Convergence time: {loaded_model.convergence_time}')
+    # convergence time
+    print(f'Convergence time: {loaded_model.convergence_time}')
 
-    # # see opinion history
-    # print(f'Opinion evolution: \n{loaded_model.X_data}')
+    # see opinion history
+    print(f'Opinion evolution: \n{loaded_model.X_data}')
 
-    # # see all edge changes, if beta is 1, no wiring since all opinions are within threshold
-    # if loaded_model.beta == 1:
-    #     print('No rewiring, beta is 1')
-    # else:
-    #     print(f'Rewiring threshold: {loaded_model.beta}')
-    #     print(f'Edge changes: \n{loaded_model.edge_changes}')
+    # see all edge changes, if beta is 1, no wiring since all opinions are within threshold
+    if loaded_model.beta == 1:
+        print('No rewiring, beta is 1')
+    else:
+        print(f'Rewiring threshold: {loaded_model.beta}')
+        print(f'Edge changes: \n{loaded_model.edge_changes}')
 
     
 
     # Plot opinion evolution
-    # plt.figure(figsize=(12, 8))
-    # plt.plot(loaded_model.X_data)   # convergence time is too 
-    # plt.xlabel('Time')
-    # plt.ylabel('Opinion')
-    # # plt.legend(['Nodes in Network'], bbox_to_anchor=(1.3, 1), loc='upper right')
-    # # plt.annotate(f'$confidence$ $\epsilon = {loaded_model.C}$', xy=(1.05,.8), xycoords='axes fraction', fontsize=12)
-    # plt.title(f'Opinion Evolution: Adaptive-BC')
-    # plt.show()
+    plt.figure(figsize=(12, 8))
+    plt.plot(loaded_model.X_data)   # convergence time is too 
+    plt.xlabel('Time')
+    plt.ylabel('Opinion')
+    # plt.legend(['Nodes in Network'], bbox_to_anchor=(1.3, 1), loc='upper right')
+    # plt.annotate(f'$confidence$ $\epsilon = {loaded_model.C}$', xy=(1.05,.8), xycoords='axes fraction', fontsize=12)
+    plt.title(f'Opinion Evolution: Adaptive-BC')
+    plt.show()
